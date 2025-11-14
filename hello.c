218964e0d3d13083d8c7cb0c347b81894c05c89a
Sirichandana1s <151798600+Sirichandana1s@users.noreply.github.com>
2025-11-14 12:30:42 +0530

    Add simple Hello World program in C
    This program prints a welcome message and project details.

---
+++ hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/* Simple Hello World Program in C */
+int main() {
+    printf("Hello, World!\n");
+    printf("This is a C program part of the HTML and C Project.\n");
+    
+    // Display project information
+    printf("\nProject Information:\n");
+    printf("- Language: C\n");
+    printf("- Purpose: Learning programming concepts\n");
+    printf("- Author: Student\n");
+    
+    return 0;
+}
